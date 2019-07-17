dfa862db2bdf4b3e2351e39d365c392b8083d2d8
smilyFw <996226235@qq.com>
2019-07-17 21:54:13 +0800

    反转链表:迭代法

---
+++ 61.旋转链表.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=61 lang=java
+ *
+ * [61] 旋转链表
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        
+    }
+}
+
