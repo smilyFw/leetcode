a82f31326cf239f3e5126aed2fcb25467345e306
smilyFw <996226235@qq.com>
2019-07-21 14:45:34 +0800

    左节点全放进栈后,怎么处理右节点逻辑,不是很熟练

---
+++ 94.二叉树的中序遍历.java
@@ -0,0 +1,39 @@
+import java.util.List;
+import java.util.Stack;
+
+/*
+ * @lc app=leetcode.cn id=94 lang=java
+ *
+ * [94] 二叉树的中序遍历
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if(root == null){
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+
+        TreeNode cur = root;
+        while(cur != null ||!stack.isEmpty()){
+            while(cur!=null){
+                stack.push(cur); 
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            result.add(cur.val);
+            cur = cur.right;
+        }
+        return result;
+    }
+}
+
