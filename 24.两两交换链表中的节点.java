c95582205b1ace344d1380f1f2bee10b1561038b
smilyFw <996226235@qq.com>
2019-11-20 21:23:47 +0800

    24

--- 24.两两交换链表中的节点.java
+++ 24.两两交换链表中的节点.java
@@ -12,31 +12,31 @@
  * }
  */
 class Solution {
-    // public ListNode swapPairs(ListNode head) {
-    //     ListNode temp = new ListNode(0);
-    //     temp.next = head;
-    //     ListNode saved = temp;
+    public ListNode swapPairs(ListNode head) {
+        ListNode temp = new ListNode(0);
+        temp.next = head;
+        ListNode saved = temp;
 
-    //     ListNode one = null;
-    //     ListNode two = null;
+        ListNode one = null;
+        ListNode two = null;
 
-    //    while(temp!=null && temp.next!=null && temp.next.next!=null){
-    //        one = temp.next;
-    //        two = temp.next.next;
-    //        one.next = two.next;
-    //        two.next = one;
-    //        temp.next = two;
-    //        temp = one;
-    //    }
-    //     return saved.next;   
-    // }
+       while(temp!=null && temp.next!=null && temp.next.next!=null){
+           one = temp.next;
+           two = temp.next.next;
+           one.next = two.next;
+           two.next = one;
+           temp.next = two;
+           temp = one;
+       }
+        return saved.next;   
+    }
 
-    public ListNode swapPairs(ListNode head){
+    public ListNode swapPairs1(ListNode head){
         if(head == null || head.next == null){
             return head;
         }
         ListNode n = head.next;
-        head.next = swapPairs(head.next.next);
+        head.next = swapPairs1(head.next.next);
         n.next = head;
         return n;
     }
